3a067bce0981d16d110b99dc75ef8cc7e008db37
thon889 <thon889@aucklanduni.ac.nz>
2017-05-11 11:50:39 +1200

    UnityProject
    The above changes are what we went through on the creative coding unity
    projects. They only show changes up to modue 5.

---
+++ CubeScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeScript : MonoBehaviour {
+
+    public Vector3 spinSpeed = Vector3.zero;
+    public float rotateSpeed = 1.0f; 
+     Vector3 spinAxis = new Vector3(0, 1, 0);
+  
+    void Start () {
+        
+        spinSpeed = new Vector3(Random.value, Random.value, Random.value);
+        spinAxis = Vector3.up;
+        spinAxis.x = (Random.value - Random.value) * .1f; 
+    }
+	
+    public void SetSize(float size)
+
+    {
+        this.transform.localScale = new Vector3(size, size, size);
+
+    }
+
+	// Update is called once per frame
+	void Update () {
+
+        this.transform.Rotate(spinSpeed);
+        this.transform.RotateAround(Vector3.zero, spinAxis, rotateSpeed);
+
+	}
+}
+    
